2ed31a04ce013013b07616a2e2652b5d45209bb0
dtran1208 <ducminh.tran08@gmail.com>
2023-04-25 00:25:09 -0400

    add all finish project to github

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # generate-readme
+# README Generator
+
+A simple command-line application to generate high-quality, professional README.md files for your projects.
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+
+## Description
+
+This README Generator prompts the user for information about their project and generates a well-structured README file with sections for Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions. It also generates license badges and provides links to the user's GitHub profile and email address for further questions.
+
+## Installation
+
+1. Clone the repository or download the source code.
+2. Navigate to the project directory.
+3. Run `npm install` to install the required dependencies (in this case, the `inquirer` package).
+
+## Usage
+
+1. In the terminal, navigate to the project directory.
+2. Run the script using `node generate-readme.js`.
+3. Follow the prompts and provide the necessary information about your project.
+4. Once completed, a `README.md` file will be generated in the same directory, containing the details you provided.
+
+## License
+
+This project is licensed under the MIT License.
+
+## Contributing
+
+If you'd like to contribute to this project, please feel free to submit a pull request or open an issue for discussion.
+
---
+++ index.js
@@ -0,0 +1,121 @@
+import inquirer from 'inquirer';
+import fs from 'fs/promises';
+
+function generateReadme(answers) {
+    return `# ${answers.title}
+
+${renderLicenseBadge(answers.license)}
+
+## Description
+
+${answers.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## License
+
+This project is covered under the ${answers.license} license.
+
+## Contributing
+
+${answers.contributing}
+
+## Tests
+
+${answers.tests}
+
+## Questions
+
+For any questions, please reach out to [${answers.github}](https://github.com/${answers.github}) or send an email to ${answers.email}.
+`;
+}
+
+function renderLicenseBadge(license) {
+    if (license === "MIT") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (license === "GPLv3") {
+        return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    } else if (license === "Apache 2.0") {
+        return "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else {
+        return "";
+    }
+}
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is your project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Enter a description for your project:",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Enter installation instructions:",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Enter usage information:",
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Enter contribution guidelines:",
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Enter test instructions:",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose a license for your application:",
+            choices: ["MIT", "GPLv3", "Apache 2.0", "None"],
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your GitHub username:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email address:",
+        },
+    ])
+    .then((answers) => {
+        // Generate the README content using the provided answers
+        const readmeContent = generateReadme(answers);
+
+        // Write the README content to a file
+        fs.writeFile("README.md", readmeContent)
+          .then(() => {
+              console.log("README.md file generated successfully!");
+          })
+          .catch((err) => {
+              console.error("Error writing README file: ", err);
+          });
+    });
